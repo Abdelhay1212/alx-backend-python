95a339b3fdd1bac1b704631c078473e2187e1806
Abdelhay1212 <110763003+Abdelhay1212@users.noreply.github.com>
2024-02-06 13:44:23 +0000

    updated

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 ''' Parameterize a unit test '''
+import utils
 import unittest
+from unittest.mock import Mock, patch
 from parameterized import parameterized
-access_nested_map = __import__('utils').access_nested_map
+from fixtures import TEST_PAYLOAD
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -14,13 +16,21 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
     def test_access_nested_map(self, nested_map, path, expected):
-        self.assertEqual(access_nested_map(nested_map, path), expected)
+        self.assertEqual(utils.access_nested_map(nested_map, path), expected)
 
     @parameterized.expand([
         ({}, ("a",), "a"),
         ({"a": 1}, ("a", "b"), "b")
     ])
     def test_access_nested_map_exception(self, nested_map, path, expected_key):
-        with self.assertRaises(KeyError) as context:
-            access_nested_map(nested_map, path)
-        self.assertIn(repr(expected_key), str(context.exception))
+        with self.assertRaises(KeyError):
+            utils.access_nested_map(nested_map, path)
+        # self.assertIn(repr(expected_key), str(context.exception))
+
+
+# class TestGetJson(unittest.TestCase):
+#     ''' Mock HTTP calls '''
+
+#     @patch()
+#     def test_get_json(self):
+#         pass
